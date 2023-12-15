653bfe97345210a9cce43f19294617b13826b72c
Tonylemty <chengshinchung31@gmail.com>
2023-12-16 00:26:27 +0800

    Using function recursion to compute fibonacci

---
+++ function/fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+
+    else {
+        return (fibonacci(n - 1) + fibonacci(n - 2)); 
+    }
+}
+
+int main() {
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", fibonacci(i));
+    }
+
+
+    return 0;
+}
